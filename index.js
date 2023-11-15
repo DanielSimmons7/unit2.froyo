43af3d500dc1899a57a3ea42a4e8313d063dc1a0
Daniel Simmons <danieltsvs@gmail.com>
2023-11-15 17:59:55 -0600

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const froyo = {
+    flavors: ['vanilla', 'strawberry', 'coffee']
+}
+
+
+
+function sumFlavors(choices){
+    let sum = 0;
+    for(let i = 0; i < choices.length; i++){
+    sum = sum + Object.values(froyo);
+    }
+    return sum;
+}
+
+function createMessage(vanilla, strawberry, coffee){
+    return `You ordered ${vanilla} vanilla, ${strawberry} strawberry, ${coffee} coffee.`
+}
+const inputFlavors = window.prompt('What flavors of Froyo would you like? Separate requests with commas, please.');
+const sumOfFlavors = sumFlavors(inputFlavors);
+console.log(createMessage(inputFlavors, sumOfFlavors));
+
